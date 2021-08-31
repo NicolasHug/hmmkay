20d64c69e6f0cde0d007ddcfc70dc87fce85742d
Daples <daples1@gmail.com>
2021-08-31 09:44:42 +0200

    [ hmmkay ] Updating to new code structure.

--- benchmark.py
+++ benchmark.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 
 from hmmkay import HMM
 from hmmkay.utils import (
-    _get_hmm_learn_model,
-    _to_weird_format,
+    get_hmm_learn_model,
+    to_weird_format,
     make_proba_matrices,
     make_observation_sequences,
 )
@@ -17,7 +17,7 @@ n_hidden_states, n_observable_states = 10, 20
 n_seq, n_obs_min, n_obs_max = 1000, 100, 150
 
 sequences = make_observation_sequences(n_seq, n_observable_states, n_obs_min, n_obs_max)
-sequences_hmmlearn = _to_weird_format(sequences)
+sequences_hmmlearn = to_weird_format(sequences)
 
 
 def _compile_code(sequences):
@@ -25,7 +25,7 @@ def _compile_code(sequences):
     print("Compiling numba code...")
     sequences = sequences[:2]
     hmm.log_likelihood(sequences)
-    hmm.decode(sequences, return_log_probas=True)
+    hmm.decode(sequences)
     hmm.fit(sequences)
     hmm.sample(2, 2)
     print("done")
@@ -36,7 +36,7 @@ init_probas, transitions, emissions = make_proba_matrices(
 )
 
 hmm = HMM(init_probas, transitions, emissions, n_iter=1)
-hmmlearn_model = _get_hmm_learn_model(hmm)
+hmmlearn_model = get_hmm_learn_model(hmm)
 
 # filter out deprecation warnings from sklearn
 simplefilter("ignore", category=DeprecationWarning)
@@ -57,7 +57,7 @@ hmmlearn_times["log_likelihood"] = time() - tic
 print(f"log_likelihood computed in {hmmlearn_times['log_likelihood']:3f} sec")
 
 tic = time()
-hmm.decode(sequences, return_log_probas=True)
+hmm.decode(sequences)
 hmmkay_times["decode"] = time() - tic
 print(f"decode computed in {hmmkay_times['decode']:3f} sec")
 
